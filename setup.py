f0515ce9d3fc222ab032aaa8458dc44d5abda807
Bark <zetatez@icloud.com>
2020-11-11 15:37:13 +0800

    update

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="evolving",
-    version="1.0.0",
+    version="1.1.1",
     author="Lorenzo",
     author_email="zetatez@icloud.com",
     description="trading api",
